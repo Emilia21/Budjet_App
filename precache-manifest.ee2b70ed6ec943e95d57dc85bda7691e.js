6479d117090638b5a457bc845e4edd157e32ebd6
Emilia21 <epetkova96@gmail.com>
2020-08-14 12:15:15 +0300

    Updates

---
+++ precache-manifest.ee2b70ed6ec943e95d57dc85bda7691e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9db8dfc55a92907cd5f8f5150ba4f8",
+    "url": "/Budjet_App/index.html"
+  },
+  {
+    "revision": "1450944dac47e76c1f6c",
+    "url": "/Budjet_App/static/css/main.c92aa993.chunk.css"
+  },
+  {
+    "revision": "75ea749eea69029eca44",
+    "url": "/Budjet_App/static/js/2.8a4d59a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budjet_App/static/js/2.8a4d59a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1450944dac47e76c1f6c",
+    "url": "/Budjet_App/static/js/main.20a48b25.chunk.js"
+  },
+  {
+    "revision": "71e657c002d23fab8b9b",
+    "url": "/Budjet_App/static/js/runtime-main.e1b3c64a.js"
+  }
+]);
